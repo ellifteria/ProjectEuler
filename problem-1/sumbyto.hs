6000df59ef572fc42a576d02c962f8d5c417070c
Elli Beres <elliberes@proton.me>
2023-04-26 12:27:32 -0500

    Add haskell versions

---
+++ problem-1/sumbyto.hs
@@ -0,0 +1,6 @@
+sumbyto start by to
+    | start > to    = 0
+    | otherwise = start + sumbyto (start + by) by to
+
+main = do
+    print (sumbyto 0 3 999 + sumbyto 0 5 999 - sumbyto 0 15 999)
---
+++ problem-2/efibsum.hs
@@ -0,0 +1,8 @@
+efibsum n nprev max
+    | n > max       = 0
+    | mod n 2 /= 0  = efibsum (n + nprev) n max
+    | otherwise     = n + efibsum (n + nprev) n max
+
+main = do
+    print(efibsum 1 1 4000000)
+
