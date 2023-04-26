14dde5d77db17c0780e6839d69ba2ecb65023f1e
Elli Beres <elliberes@proton.me>
2023-04-26 12:35:51 -0500

    Solve 6 in haskell

---
+++ problem-6/sumsqrdiff.hs
@@ -0,0 +1,10 @@
+sumsqrto n max
+    | n >  max  = 0
+    | otherwise = (n * n) + sumsqrto (n + 1) max
+
+sumto n  max
+    | n > max   = 0
+    | otherwise = n + sumto (n + 1) max
+
+main = do
+    print( (sumto 0 100) ^ 2 - sumsqrto 0 100)
