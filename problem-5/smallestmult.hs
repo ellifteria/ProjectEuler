5cb5161e80af792ad40a5fd746d8f12c55b28dd9
Elli Beres <elliberes@proton.me>
2023-04-26 15:36:15 -0500

    Solve 5 in haskell

---
+++ problem-5/smallestmult.hs
@@ -0,0 +1,17 @@
+reduceby n by
+    | by == 1           = n
+    | by == 0           = 1
+    | (mod n by) == 0   = reduceby (div n by) ((reduceby by (by - 1)) - 1)
+    | otherwise         = reduceby n (by - 1)
+
+needed :: Int -> [Int]
+needed n
+    | n == 1    = [1]
+    | otherwise = (reduceby n (n - 1)):(needed (n - 1))
+
+listprod list
+    | (tail list) == [] = head list
+    | otherwise         = (head list) * (listprod (tail list))
+
+main = do
+    print(listprod (needed 20))
